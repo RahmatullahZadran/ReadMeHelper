a59bced9f45f9c73453d3d38efb2915416d2d383
Rahmatullah <Rahmatullahzadran@outlook.com>
2024-05-23 23:39:32 +0100

    first commit -m

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+
+import './App.css'
+
+import './index.css'
+
+import Navbar from '../src/components/navbat.jsx'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Navbar />
+      </div>
+     
+    </>
+  )
+}
+
+export default App
